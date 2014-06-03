e424cf1054d6e52714d09b92b92742bc761b274c
Saleh Kaddoura <saleh.kaddoura@gmail.com>
2014-06-03 14:52:06 -0700

    revamped the whole site added a new slideshow, changed colors beter directory structure

--- js/script.js
+++ js/script.js
@@ -1,26 +1,22 @@
-image1 = new Image()
-image1.src="pics/limo_black.jpg"
-image2 = new Image()
-image2.src="pics/slide_camry.jpg"
-image3 = new Image()
-image3.src ="pics/slide_limo.jpg"
-image4 = new Image()
-image4.src ="pics/limo3.jpg"
-image5 = new Image()
-image5.src ="pics/instcar4.jpg"
-image6 = new Image()
-image6.src ="pics/instcar5.jpg"
+$(document).ready(function(){
+	$('.slideShow').slick({
+		centerMode: true,
+        infinite: true,
+        autoplay: true,
+        speed: 500,
+        fade: true,
+        slide: 'div',
+        cssEase: 'linear'
+	});
 
-var step = 1
-function slideit(){
-	if (!document.images)
-		return
-	document.images.slide.src = eval("image"+step+".src");
-		if (step<9)
-			step++;
-		else
-			step = 1;
-	setTimeout("slideit()",2500);
+	$('.banner').slick({
+		infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 700,
+        arrows: false
+      });
+});
 
-}
-slideit();
+			
+			
